057a14b8d97ef0f07ea7d99a38e2cd1b7f6ed242
Buddha Lo <lo_977@yahoo.com>
2024-02-23 13:35:33 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+// //   ,,,,,
+// function superbowlWin(record) {
+//     const newResults = record.find(obj => obj.result === 'W');
+//     return newResults? newResults.year : undefined;
+//    }
+
+const superbowlWin = (array) => {
+  let newArray = array.find(subArray => subArray.result === "W");
+  return newArray? newArray.year : undefined;
+};

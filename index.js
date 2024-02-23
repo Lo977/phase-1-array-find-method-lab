8ed7705a48f7f0472025d39ff96b77e4636ce9b5
Buddha Lo <lo_977@yahoo.com>
2024-02-23 13:37:06 -0800

    complete lab

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const record = [
   { year: "2013", result: "L" },
   //...
 ];
-// //   ,,,,,
-// function superbowlWin(record) {
-//     const newResults = record.find(obj => obj.result === 'W');
-//     return newResults? newResults.year : undefined;
-//    }
 
 const superbowlWin = (array) => {
-  let newArray = array.find(subArray => subArray.result === "W");
-  return newArray? newArray.year : undefined;
+  let newArray = array.find((subArray) => subArray.result === "W");
+  return newArray ? newArray.year : undefined;
 };
